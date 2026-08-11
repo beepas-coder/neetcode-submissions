b1fcb13bd763266dc51d0445be59c2dfc0a145b4
beepas-coder <ianbeepas@gmail.com>
2026-08-11 16:00:21 +0300

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        time = []
+        for i in intervals:
+            time.append((i.start, 1))
+            time.append((i.end, -1))
+        time.sort(key=lambda x: (x[0], x[1]))
+        res = count = 0
+        for t in time:
+            count += t[1]
+            res = max(res, count)
+        return res
+
